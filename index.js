2c8e12b10e7afd15aef76fb5cca41ef937e5f180
Mr-Kasa <156632133+Mr-Kasa@users.noreply.github.com>
2024-05-06 13:16:56 +0200

    all functionalities have been implemented and are functional

--- index.js
+++ index.js
@@ -6,27 +6,29 @@
  */
 
 // Given Parameters
-const vel = 10000; // velocity (km/h)
-const acc = 3; // acceleration (m/s^2)
+const initialVelocity = 10000; // velocity (km/h)
+const acceleration = {metersPerSecond:3,
+                      kilometersPerHour:38880}     // acceleration (km/h^2)
 const time = {seconds : 3600,    //3600 seconds equal to 1 hour
               hours : 1}; 
 const distance = 0; // distance (km)
-const fuel = 5000; // remaining fuel (kg)
-const fbr = 0.5; // fuel burn rate (kg/s)
-console.log(time.hours)
-
-const d2 = distance + (vel*time.hours) //calcultes new distance
-const rf = fbr*time.seconds //calculates remaining fuel
-//const vel2 = calcNewVel(acc, vel, time) //calculates new velocity based on acceleration
+let Startingfuel = 5000; // remaining fuel (kg)
+const fuelBurnRate = {kgPerSecond: 0.5,                         // fuel burn rate (kg/s)
+                      kgPerHour: 1800}                           //kg per hour
+const secondDistance = distance + (initialVelocity*time.hours) //calcultes new distance
+const remainingFuel =Startingfuel - fuelBurnRate.kgPerHour*time.hours //calculates remaining fuel
+const secondVelocity = calculateNewVelocity(acceleration.kilometersPerHour, initialVelocity, time.hours) //calculates new velocity based on acceleration
 
 // Pick up an error with how the function below is called and make it robust to such errors
-calcNewVel = (vel, acc, time) => { 
-  return vel + (acc*time)
+function calculateNewVelocity(acc, vel, time){ 
+
+ return parseFloat(vel + (acc*time))
+  
 }
 
-console.log(`Corrected New Velocity: ${vel2} km/h`);
-console.log(`Corrected New Distance: ${d2} km`);
-console.log(`Corrected Remaining Fuel: ${rf} kg`);
+console.log(`Corrected New Velocity: ${secondVelocity} km/h`);
+console.log(`Corrected New Distance: ${secondDistance} km`);
+console.log(`Corrected Remaining Fuel: ${remainingFuel} kg`);
 
 
 
